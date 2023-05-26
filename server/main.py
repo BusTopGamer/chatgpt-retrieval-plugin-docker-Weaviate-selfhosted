9832d11269d75c761e04cce7fd5022be5dea4b15
BusTopGamer <shakeel1985@gmail.com>
2023-05-26 15:59:25 -0600

    Update main.py

--- server/main.py
+++ server/main.py
@@ -152,4 +152,4 @@ async def startup():
 
 
 def start():
-    uvicorn.run("server.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("server.main:app", host="0.0.0.0", port=5000, reload=True)
